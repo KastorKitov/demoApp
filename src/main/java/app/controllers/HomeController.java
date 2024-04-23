e7e43b1c691073a483a73f3e4dd707f15c493db4
Kastor Kitov <kitov.kastor@gmail.com>
2024-04-23 12:26:11 +0300

    added car model and html/css

--- src/main/java/app/controllers/HomeController.java
+++ src/main/java/app/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package app.controllers;
 
 import app.models.Car;
+import org.springframework.boot.Banner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndViewDefiningException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +22,7 @@ public class HomeController {
 
     @ResponseBody
     @RequestMapping("/cars")
-    public ResponseEntity<List<Car>> car(){
+    public ModelAndView car(ModelAndView modelAndView){
         Car car1 = new Car("OPEL", "ASTRA", "5800");
         Car car2 = new Car("BMW", "E90", "12000");
         Car car3 = new Car("MAZDA", "6", "7800");
@@ -29,7 +32,9 @@ public class HomeController {
         cars.add(car2);
         cars.add(car3);
 
-        return ResponseEntity.ok(cars);
+        modelAndView.setViewName("index");
+        modelAndView.addObject("cars", cars);
+        return modelAndView;
 
     }
 }
